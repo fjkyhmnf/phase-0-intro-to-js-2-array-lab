043414a557dd7703046f8f088144d214d5155d2c
Hung Ngo <hung.a.ngo@gmail.com>
2022-04-08 17:37:37 -0500

    commit tree 13477e63bce487d14c878c43d3afeda260dd45a4

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    cats.push(name)
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(name)
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop()
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+}
+
+function appendCat(name){
+    const AppendCat = [...cats, "Broom"]
+    return AppendCat
+}
+
+function prependCat(name){
+    const PrependCat = ["Arnold", ...cats]
+    return PrependCat
+}
+
+function removeLastCat(){
+    return cats.slice(0, 2)
+}
+
+function removeFirstCat(){
+    return cats.slice(1)
+}
